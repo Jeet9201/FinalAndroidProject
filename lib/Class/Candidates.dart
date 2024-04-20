f29100e533c510f2d7c482fe50525aa8cd91bba8
sahil-pat <144398671+sahil-pat@users.noreply.github.com>
2024-04-19 23:47:42 -0400

    Add files via upload

---
+++ lib/Class/Candidates.dart
@@ -0,0 +1,14 @@
+class Candidate{
+  String name;
+  String description;
+  String image;
+  String email;
+  String qualification;
+  String skillID1Name;
+  String skillID2Name;
+  String skillId1Level;
+  String skillId2Level;
+
+  Candidate(this.name,this.description,this.image,this.email,this.qualification,this.skillID1Name,this.skillID2Name,this.skillId1Level,this.skillId2Level);
+}
+
---
+++ lib/Class/Jobs.dart
@@ -0,0 +1,13 @@
+class Jobs{
+  String company;
+  String jobDescription;
+  String jobQualification;
+  String jobMail;
+  String jobname;
+  String joblocation;
+  String salary;
+  String jobImage;
+
+  Jobs(this.company,this.jobDescription,this.jobQualification,this.jobMail,this.jobname,this.joblocation,this.salary,this.jobImage);
+}
+
